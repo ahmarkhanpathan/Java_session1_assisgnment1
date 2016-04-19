2fde798ea08e303180b63301f69c713c59efd62f
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-04-19 18:31:58 +0530

    Assignment 1
    This program shows the sum of an integer and a float to give the answer
    as an integer.

---
+++ src/package1/Page1.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class Page1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	String note="This program shows the sum of a float and an integer and answer is an integer";
+	
+	float a;
+	int b;
+	Integer c;
+		
+		a = 2.7f;
+		b = 10;
+		c = (int)(a + b);
+		
+				
+	System.out.println(note);
+	 
+	System.out.println(c);
+	
+		
+	}
+
+}
